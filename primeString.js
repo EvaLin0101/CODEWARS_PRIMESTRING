618aa663028af4c54121aecb870e430893d2612c
Eva Lin <EvaLin0101@users.noreply.github.com>
2017-08-01 18:16:12 +0800

    修正錯誤

--- primeString.js
+++ primeString.js
@@ -1,7 +1,8 @@
 function primeString(s) {
       var stringLength = s.length;
-            for(let i=2; i<= stringLength; i++){
-                  if(s.slice(0,stringLength/i).repeat(i)==s)
-            }
+      for(let i=2; i<= stringLength; i++){
+            if(s.slice(0,stringLength/i).repeat(i)==s)
+                  return false
+      }
       return true;
  }
