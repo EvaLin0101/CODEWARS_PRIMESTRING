05ad4c39db6e751110eff486cd19ee31b25ad18a
Eva Lin <EvaLin0101@users.noreply.github.com>
2017-08-01 18:13:50 +0800

    rename

---
+++ primeString.js
@@ -0,0 +1,7 @@
+function primeString(s) {
+      var stringLength = s.length;
+            for(let i=2; i<= stringLength; i++){
+                  if(s.slice(0,stringLength/i).repeat(i)==s)
+            }
+      return true;
+ }
